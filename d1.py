8fb3f1550127d50d6bf0a523ec4a423ee7578da8
Alexa Dias <110355804+alexaiung@users.noreply.github.com>
2025-04-11 17:22:33 -0300

    Commit inicial
    Arquivo inicial com código da primeira aula

---
+++ d1.py
@@ -0,0 +1,6 @@
+# Simple program to concatenate strings.
+# More interesting options would use random function, which I will explore in later exercises.
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+print(f'Your band name could be {city} {pet}')
